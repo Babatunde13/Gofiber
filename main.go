72b63acf59a155547a90c7c05308e72a08c60453
Babatunde Koiki <koikibabatunde14@gmail.com>
2022-01-01 00:49:48 +0100

    switch env var from DB_PASSWORD to DB_PASS

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main () {
 		Host:     os.Getenv("DB_HOST"),
 		Port:     os.Getenv("DB_PORT"),
 		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
+		Password: os.Getenv("DB_PASS"),
 		DBName:   os.Getenv("DB_NAME"),
 		SSLMode:  os.Getenv("DB_SSL_MODE"),
 	}
